f36deb2a4e735c1d097b8daf8441906e492e96ec
alexander_mosiyuk <titanproject21@gmail.com>
2022-02-17 22:27:31 +0200

    new folders scss-css-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import './components/css/styleZero.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hi
+        
     </div>
   );
 }
