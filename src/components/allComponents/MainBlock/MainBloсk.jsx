ac70987cf394307a57328bf41ec1671eb393cad9
alexander_mosiyuk <titanproject21@gmail.com>
2022-02-21 18:43:42 +0200

    two new components(StudentPage and MainLessons)

--- src/components/allComponents/MainBlock/MainBloсk.jsx
+++ src/components/allComponents/MainBlock/MainBloсk.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import style from "./mainBloсk.module.scss";
 import Sidebar from './SideBar/Sidebar';
-import MainContainer from './MainContainer/MainContainer';
+import StudentPage from './StudentPage/StudentPage';
+import MainLessons from './MainLessons/MainLessons';
 
 const MainBlock = () => {
     return (
-        <main className={style.main}>
-            <Sidebar />
-            <MainContainer/>
-        </main>
-    )
+    <main className={style.main}>
+        <Sidebar />
+        <StudentPage />
+        {/*<MainLessons />*/}
+    </main>
+    );
 };
 
 export default MainBlock;
